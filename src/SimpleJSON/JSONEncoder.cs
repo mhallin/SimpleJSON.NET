8f169f877356c45774de6c484e913fc577624029
mhallin <mhallin@gmail.com>
2011-09-01 13:24:27 +0200

    Removed unused imports.

--- src/SimpleJSON/JSONEncoder.cs
+++ src/SimpleJSON/JSONEncoder.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace SimpleJSON {
     public class JSONEncoder {
