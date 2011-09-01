b4ba6d99025cfe431abb421444cc804f0de523fa
mhallin <mhallin@gmail.com>
2011-09-01 10:41:16 +0200

    Implemented a simple JSON serializer.
    - Serializes primitive types, dictionaries and enumerables
    - Unit tested with NUnit.

---
+++ src/SimpleJSON/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleJSON")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Boldai")]
+[assembly: AssemblyProduct("SimpleJSON")]
+[assembly: AssemblyCopyright("Copyright © Boldai 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2443af9d-04cf-4447-8bf3-bba90d6258a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Tests.SimpleJSON/EncoderTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleJSON;
+
+namespace Tests.SimpleJSON {
+    [TestFixture]
+    public class EncoderTests {
+        private Dictionary<string, string> _stringTestCases;
+
+        [SetUp]
+        public void SetUp() {
+            _stringTestCases = new Dictionary<string, string>
+                                   {
+                                       { "string", "\"string\"" },
+                                       { "\" \\ \b \f \n \r \t", "\"\\\" \\\\ \\b \\f \\n \\r \\t\"" },
+                                       { "\u03A0", "\"\\u03A0\"" },
+                                       { "\0", "\"\\u0000\"" },
+                                       { "\U0001d120", "\"\\uD834\\uDD20\"" }
+                                   };
+        }
+
+        [Test]
+        public void String() {
+            foreach (var pair in _stringTestCases) {
+                Assert.AreEqual(pair.Value, EncodeObject(pair.Key));
+            }
+        }
+
+        [Test]
+        public void Int() {
+            Assert.AreEqual("123", EncodeObject(123));
+            Assert.AreEqual("2147483647", EncodeObject(Int32.MaxValue));
+            Assert.AreEqual("-2147483648", EncodeObject(Int32.MinValue));
+        }
+
+        [Test]
+        public void UInt() {
+            Assert.AreEqual("123", EncodeObject(123U));
+            Assert.AreEqual("4294967295", EncodeObject(UInt32.MaxValue));
+        }
+
+        [Test]
+        public void Long() {
+            Assert.AreEqual("123", EncodeObject(123L));
+            Assert.AreEqual("9223372036854775807", EncodeObject(Int64.MaxValue));
+            Assert.AreEqual("-9223372036854775808", EncodeObject(Int64.MinValue));
+        }
+
+        [Test]
+        public void ULong() {
+            Assert.AreEqual("123", EncodeObject(123UL));
+            Assert.AreEqual("18446744073709551615", EncodeObject(UInt64.MaxValue));
+        }
+
+        [Test]
+        public void Double() {
+            Assert.AreEqual("1.5", EncodeObject(1.5));
+            Assert.AreEqual("1000000", EncodeObject(1.0e6));
+            Assert.AreEqual("-1000000", EncodeObject(-1.0e6));
+            Assert.AreEqual("5E-06", EncodeObject(5.0e-6));
+        }
+
+        [Test]
+        public void Float() {
+            Assert.AreEqual("1.5", EncodeObject(1.5f));
+            Assert.AreEqual("1000000", EncodeObject(1.0e6f));
+            Assert.AreEqual("-1000000", EncodeObject(-1.0e6f));
+            Assert.AreEqual("5E-05", EncodeObject(5.0e-5f));
+        }
+
+        [Test]
+        public void Null() {
+            Assert.AreEqual("null", EncodeObject(null));
+        }
+
+        [Test]
+        public void Boolean() {
+            Assert.AreEqual("true", EncodeObject(true));
+            Assert.AreEqual("false", EncodeObject(false));
+        }
+
+        [Test]
+        public void Array() {
+            Assert.AreEqual("[1,2,3]", EncodeObject(new[] { 1, 2, 3 }));
+            Assert.AreEqual("[[],\"str\",1.5]", EncodeObject(new object[] { new object[0], "str", 1.5 }));
+        }
+
+        [Test]
+        public void Dictionary() {
+            Assert.AreEqual("{\"X\":10,\"Y\":20}",
+                            EncodeObject(new Dictionary<string, float> { { "X", 10 }, { "Y", 20 } }));
+        }
+
+        private static string EncodeObject(object obj) {
+            return JSONEncoder.Encode(obj);
+        }
+    }
+}
