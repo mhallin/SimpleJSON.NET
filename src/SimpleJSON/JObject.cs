e312d78b605f3969dc5e9dd2f86f830a4906447a
mhallin <mhallin@gmail.com>
2011-09-01 13:20:19 +0200

    Implemented a simple JSON decoder
    The decoder decodes JSON into JObjects, which contains the
    value along with some type information.
    
    JObjects can be cast to many primitive types (but not all, e.g.
    decimal), and a number is populated in all fields big enough
    to contain the number.

---
+++ src/SimpleJSON/JObject.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SimpleJSON {
+    public enum JObjectKind {
+        Object,
+        Array,
+        String,
+        Number,
+        Boolean,
+        Null
+    }
+
+    public enum IntegerSize {
+        UInt64,
+        Int64,
+        UInt32,
+        Int32,
+        UInt16,
+        Int16,
+        UInt8,
+        Int8,
+    }
+
+    public enum FloatSize {
+        Double,
+        Single
+    }
+
+    public class JObject {
+        public JObjectKind Kind { get; private set; }
+
+        public Dictionary<string, JObject> ObjectValue { get; private set; }
+        public List<JObject> ArrayValue { get; private set; }
+        public string StringValue { get; private set; }
+        public bool BooleanValue { get; private set; }
+
+        public int Count {
+            get {
+                return Kind == JObjectKind.Array ? ArrayValue.Count
+                     : Kind == JObjectKind.Object ? ObjectValue.Count
+                     : 0;
+            }
+        }
+
+        public double DoubleValue { get; private set; }
+        public float FloatValue { get; private set; }
+        public ulong ULongValue { get; private set; }
+        public long LongValue { get; private set; }
+        public uint UIntValue { get; private set; }
+        public int IntValue { get; private set; }
+        public ushort UShortValue { get; private set; }
+        public short ShortValue { get; private set; }
+        public byte ByteValue { get; private set; }
+        public sbyte SByteValue { get; private set; }
+
+        public bool IsNegative { get; private set; }
+        public bool IsFractional { get; private set; }
+        public IntegerSize MinInteger { get; private set; }
+        public FloatSize MinFloat { get; private set; }
+
+        public JObject this[string key] { get { return ObjectValue[key]; } }
+        public JObject this[int key] { get { return ArrayValue[key]; } }
+
+        public static explicit operator string(JObject obj) { return obj.StringValue; }
+        public static explicit operator bool(JObject obj) { return obj.BooleanValue; }
+        public static explicit operator double(JObject obj) { return obj.DoubleValue; }
+        public static explicit operator float(JObject obj) { return obj.FloatValue; }
+        public static explicit operator ulong(JObject obj) { return obj.ULongValue; }
+        public static explicit operator long(JObject obj) { return obj.LongValue; }
+        public static explicit operator uint(JObject obj) { return obj.UIntValue; }
+        public static explicit operator int(JObject obj) { return obj.IntValue; }
+        public static explicit operator ushort(JObject obj) { return obj.UShortValue; }
+        public static explicit operator short(JObject obj) { return obj.ShortValue; }
+        public static explicit operator byte(JObject obj) { return obj.ByteValue; }
+        public static explicit operator sbyte(JObject obj) { return obj.SByteValue; }
+
+        public static JObject CreateString(string str) { return new JObject(str); }
+        public static JObject CreateBoolean(bool b) { return new JObject(b); }
+        public static JObject CreateNull() { return new JObject(); }
+        public static JObject CreateNumber(string integer, string frac, string exp) { return new JObject(integer, frac, exp); }
+        public static JObject CreateArray(List<JObject> list) { return new JObject(list); }
+        public static JObject CreateObject(Dictionary<string, JObject> dict) { return new JObject(dict); }
+
+        private JObject(string str) {
+            Kind = JObjectKind.String;
+            StringValue = str;
+        }
+
+        private JObject(bool b) {
+            Kind = JObjectKind.Boolean;
+            BooleanValue = b;
+        }
+
+        private JObject() {
+            Kind = JObjectKind.Null;
+        }
+
+        private JObject(string integer, string frac, string exp) {
+            Kind = JObjectKind.Number;
+            if (frac == "" && exp == "") {
+                MakeInteger(integer);
+            } else {
+                MakeFloat(integer, frac, exp);
+            }
+        }
+
+        private JObject(List<JObject> list) {
+            Kind = JObjectKind.Array;
+            ArrayValue = list;
+        }
+
+        private JObject(Dictionary<string, JObject> dict) {
+            Kind = JObjectKind.Object;
+            ObjectValue = dict;
+        }
+
+        private void MakeInteger(string integer) {
+            if (integer[0] == '-') {
+                LongValue = Int64.Parse(integer, CultureInfo.InvariantCulture);
+                IsNegative = true;
+                MinInteger = IntegerSize.Int64;
+
+                if (LongValue >= Int32.MinValue) {
+                    IntValue = (int)LongValue;
+                    MinInteger = IntegerSize.Int32;
+                }
+
+                if (LongValue >= Int16.MinValue) {
+                    ShortValue = (short)LongValue;
+                    MinInteger = IntegerSize.Int16;
+                }
+
+                if (LongValue >= SByte.MinValue) {
+                    SByteValue = (sbyte)LongValue;
+                    MinInteger = IntegerSize.Int8;
+                }
+
+                DoubleValue = LongValue;
+                MinFloat = FloatSize.Double;
+
+                if (DoubleValue >= -Single.MaxValue) {
+                    FloatValue = (float)DoubleValue;
+                    MinFloat = FloatSize.Single;
+                }
+            } else {
+                ULongValue = UInt64.Parse(integer, CultureInfo.InvariantCulture);
+                IsNegative = false;
+                MinInteger = IntegerSize.UInt64;
+
+                if (ULongValue <= Int64.MaxValue) {
+                    LongValue = (long)ULongValue;
+                    MinInteger = IntegerSize.Int64;
+                }
+
+                if (ULongValue <= UInt32.MaxValue) {
+                    UIntValue = (uint)ULongValue;
+                    MinInteger = IntegerSize.UInt32;
+                }
+
+                if (ULongValue <= Int32.MaxValue) {
+                    IntValue = (int)ULongValue;
+                    MinInteger = IntegerSize.Int32;
+                }
+
+                if (ULongValue <= UInt16.MaxValue) {
+                    UShortValue = (ushort)ULongValue;
+                    MinInteger = IntegerSize.UInt16;
+                }
+
+                if (ULongValue <= (ulong)Int16.MaxValue) {
+                    ShortValue = (short)ULongValue;
+                    MinInteger = IntegerSize.Int16;
+                }
+
+                if (ULongValue <= Byte.MaxValue) {
+                    ByteValue = (byte)ULongValue;
+                    MinInteger = IntegerSize.UInt8;
+                }
+
+                if (ULongValue <= (ulong)SByte.MaxValue) {
+                    SByteValue = (sbyte)ULongValue;
+                    MinInteger = IntegerSize.Int8;
+                }
+
+                DoubleValue = ULongValue;
+                MinFloat = FloatSize.Double;
+
+                if (DoubleValue <= Single.MaxValue) {
+                    FloatValue = (float)DoubleValue;
+                    MinFloat = FloatSize.Single;
+                }
+            }
+        }
+
+        private void MakeFloat(string integer, string frac, string exp) {
+            DoubleValue = Double.Parse(integer + frac + exp, CultureInfo.InvariantCulture);
+            MinFloat = FloatSize.Double;
+            IsFractional = true;
+
+            if (DoubleValue < 0) {
+                IsNegative = true;
+
+                if (DoubleValue >= -Single.MaxValue) {
+                    FloatValue = (float)DoubleValue;
+                    MinFloat = FloatSize.Single;
+                }
+            } else if (DoubleValue <= Single.MaxValue) {
+                FloatValue = (float)DoubleValue;
+                MinFloat = FloatSize.Single;
+            }
+        }
+    }
+}
---
+++ src/SimpleJSON/JSONDecoder.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimpleJSON {
+    public static class JSONDecoder {
+        private const char ObjectStart = '{';
+        private const char ObjectEnd = '}';
+        private const char ObjectPairSeparator = ',';
+        private const char ObjectSeparator = ':';
+        private const char ArrayStart = '[';
+        private const char ArrayEnd = ']';
+        private const char ArraySeparator = ',';
+        private const char StringStart = '"';
+        private const char NullStart = 'n';
+        private const char TrueStart = 't';
+        private const char FalseStart = 'f';
+
+        private static readonly Regex NumberRegex = new Regex("(-?(?:0|[1-9]\\d*))(\\.\\d+)?([eE][-+]?\\d+)?",
+                                                              RegexOptions.CultureInvariant | RegexOptions.Multiline);
+
+        public static JObject DecodeJSON(string json) {
+            var data = Scan(json, 0);
+            return data.Result;
+        }
+
+        private struct ScannerData {
+            public readonly JObject Result;
+            public readonly int Index;
+
+            public ScannerData(JObject result, int index) {
+                Result = result;
+                Index = index;
+            }
+        }
+
+        private static readonly Dictionary<char, string> EscapeChars =
+            new Dictionary<char, string>
+                {
+                    { '"', "\"" },
+                    { '\\', "\\" },
+                    { 'b', "\b" },
+                    { 'f', "\f" },
+                    { 'n', "\n" },
+                    { 'r', "\r" },
+                    { 't', "\t" },
+                };
+
+        private static ScannerData Scan(string json, int index) {
+            var nextChar = json[index];
+
+            switch (nextChar) {
+            case StringStart:
+                return ScanString(json, index + 1);
+            case TrueStart:
+                return ScanTrue(json, index);
+            case FalseStart:
+                return ScanFalse(json, index);
+            case NullStart:
+                return ScanNull(json, index);
+            case ArrayStart:
+                return ScanArray(json, index);
+            case ObjectStart:
+                return ScanObject(json, index);
+            default:
+                if (IsNumberStart(nextChar)) {
+                    return ScanNumber(json, index);
+                }
+                throw new ArgumentException("Invalid JSON", "json");
+            }
+        }
+
+        private static ScannerData ScanString(string json, int index) {
+            var strBuilder = new StringBuilder();
+            while (json[index] != '"') {
+                if (json[index] == '\\') {
+                    ++index;
+                    if (EscapeChars.ContainsKey(json[index])) {
+                        strBuilder.Append(EscapeChars[json[index]]);
+                        ++index;
+                    } else if (json[index] == 'u') {
+                        ++index;
+                        var unicodeSequence = Convert.ToInt32(json.Substring(index, 4), 16);
+                        strBuilder.Append((char)unicodeSequence);
+                        index += 4;
+                    }
+                } else {
+                    strBuilder.Append(json[index]);
+                    ++index;
+                }
+            }
+            return new ScannerData(JObject.CreateString(strBuilder.ToString()), index + 1);
+        }
+
+        private static ScannerData ScanTrue(string json, int index) {
+            return new ScannerData(JObject.CreateBoolean(true), ExpectConstant(json, index, "true"));
+        }
+
+        private static ScannerData ScanFalse(string json, int index) {
+            return new ScannerData(JObject.CreateBoolean(false), ExpectConstant(json, index, "false"));
+        }
+
+        private static ScannerData ScanNull(string json, int index) {
+            return new ScannerData(JObject.CreateNull(), ExpectConstant(json, index, "null"));
+        }
+
+        private static ScannerData ScanNumber(string json, int index) {
+            var match = NumberRegex.Match(json, index);
+
+            if (!match.Success) {
+                throw new ArgumentException("Could not parse number", "json");
+            }
+
+            return new ScannerData(JObject.CreateNumber(match.Groups[1].Value,
+                                                        match.Groups[2].Value,
+                                                        match.Groups[3].Value), index + match.Groups[0].Length);
+        }
+
+        private static ScannerData ScanArray(string json, int index) {
+            var list = new List<JObject>();
+
+            if (json[index + 1] == ArrayEnd) return new ScannerData(JObject.CreateArray(list), index + 2);
+
+            while (json[index] != ArrayEnd) {
+                ++index;
+                var result = Scan(json, index);
+                index = result.Index;
+                if (json[index] != ArraySeparator && json[index] != ArrayEnd) {
+                    throw new ArgumentException("Expecting array separator (,) or array end (])", "json");
+                }
+                list.Add(result.Result);
+            }
+            return new ScannerData(JObject.CreateArray(list), index + 1);
+        }
+
+        private static ScannerData ScanObject(string json, int index) {
+            var dict = new Dictionary<string, JObject>();
+
+            if (json[index + 1] == ObjectEnd) return new ScannerData(JObject.CreateObject(dict), index + 1);
+
+            while (json[index] != ObjectEnd) {
+                ++index;
+                var keyResult = Scan(json, index);
+                if (keyResult.Result.Kind != JObjectKind.String) {
+                    throw new ArgumentException("Object keys must be strings", "json");
+                }
+                index = keyResult.Index;
+                if (json[index] != ObjectSeparator) {
+                    throw new ArgumentException("Expecting object separator (:)", "json");
+                }
+                ++index;
+                var valueResult = Scan(json, index);
+                index = valueResult.Index;
+                if (json[index] != ObjectEnd && json[index] != ObjectPairSeparator) {
+                    throw new ArgumentException("Expecting object pair separator (,) or object end (})");
+                }
+                dict[keyResult.Result.StringValue] = valueResult.Result;
+            }
+            return new ScannerData(JObject.CreateObject(dict), index + 1);
+        }
+
+        private static int ExpectConstant(string json, int index, string expected) {
+            if (json.Substring(index, expected.Length) != expected) {
+                throw new ArgumentException(string.Format("Expected '{0}' got '{1}'",
+                                                          expected,
+                                                          json.Substring(index, expected.Length)));
+            }
+            return index + expected.Length;
+        }
+
+        private static bool IsNumberStart(char b) {
+            return b == '-' || (b >= '1' && b <= '9');
+        }
+    }
+}
